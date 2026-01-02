106097facec355de5f676bbd58c61ac377a87616
IacobetiDan <IacobetiDan@hotmail.com>
2026-01-02 17:38:25 +0200

    rate calculator

--- GoLang/investment_calculator.go
+++ GoLang/investment_calculator.go
@@ -6,10 +6,44 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Print("Ivestment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println((futureRealValue))
+
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
 }
