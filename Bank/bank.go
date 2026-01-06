9ddf4e8ee236247e89d2665a9e1cc265effa3dc2
IacobetiDan <IacobetiDan@hotmail.com>
2026-01-06 19:48:31 +0200

    Bank app

---
+++ Bank/bank.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance float64 = 1000
+
+	fmt.Println("Welcome to go Bank!")
+	fmt.Println("What if you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice here: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is: ", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance = accountBalance + depositAmount
+		//accountBalance += depositAmount
+		fmt.Println("Balance update! New amount: ", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Your withdraw: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+		accountBalance = accountBalance - withdrawAmount
+		//accountBalance += depositAmount
+		fmt.Println("Balance update! New amount: ", accountBalance)
+	} else {
+		fmt.Println("Good bye!")
+	}
+
+	fmt.Println("Your choice:", choice)
+}
